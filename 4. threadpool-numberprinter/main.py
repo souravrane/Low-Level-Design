8b2a7a93c25457b622f1e54a122651e63d23efd4
Sourav Rane <souravrane112@gmail.com>
2025-01-18 18:18:56 +0530

    Cached thread pool

--- 4. threadpool-numberprinter/main.py
+++ 4. threadpool-numberprinter/main.py
@@ -2,8 +2,9 @@ from customThreadPool import CustomThreadPool
 from numberPrinter import NumberPrinter
 
 if __name__ == "__main__":
+    # If you want to turn the thread pool into cached thread pool, use max_workers = None
     threadPool = CustomThreadPool(5)
-    for i in range(10):
+    for i in range(100):
         task = NumberPrinter(i)
         threadPool.submit(task)
     
